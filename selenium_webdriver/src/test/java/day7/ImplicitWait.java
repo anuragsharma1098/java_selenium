9db42d405c7c9b7d0a578e60a897b5f77e544399
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-09-14 17:52:12 +0530

    .

---
+++ selenium_webdriver/src/test/java/day7/ImplicitWait.java
@@ -0,0 +1,12 @@
+package day7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+    public static void main(String[] args) {
+        System.out.println("ImplicitWait");
+        WebDriver driver = new ChromeDriver();
+        
+    }
+}
