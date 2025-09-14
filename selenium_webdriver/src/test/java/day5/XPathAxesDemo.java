680b83c7981b9053c4ce9b97d9ba9c374b7b1200
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-09-14 10:52:16 +0530

    axes

--- selenium_webdriver/src/test/java/day5/XPathAxesDemo.java
+++ selenium_webdriver/src/test/java/day5/XPathAxesDemo.java
@@ -20,8 +20,22 @@ public class XPathAxesDemo {
         driver.navigate().refresh();
         Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/div[4]/div/span")).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         driver.quit();
+
+        // xpath axises
+        // self, parent, child, ancestor, descendant, following, preceding,
+        // following-sibling, preceding-sibling
+
+        // self: //input[@name='q']/self::input
+        // parent: //input[@name='q']/parent::div
+        // child: //div[@class='_1kidPb']/child::input
+        // ancestor: //input[@name='q']/ancestor::div
+        // descendant: //div[@class='_1kidPb']/descendant::input
+        // following: //input[@name='q']/following::button
+        // preceding: //button[@class='L0Z3Pu']/preceding::input
+        // following-sibling: //input[@name='q']/following-sibling::button
+        // preceding-sibling: //button[@class='L0Z3Pu']/preceding-sibling::input
 
     }
 }
