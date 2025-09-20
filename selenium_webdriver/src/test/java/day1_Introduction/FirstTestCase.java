d917a7850ec7932d21751f161aa75a673228f251
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-09-20 21:30:59 +0530

    .

---
+++ selenium_webdriver/src/test/java/day1_Introduction/FirstTestCase.java
@@ -0,0 +1,47 @@
+package day1_Introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+/*
+ 1. Open Chrome Browser
+ 2. Go to https://www.google.com/
+ 3. Validate the title is "Google"
+ 4. Close the browser
+ */
+public class FirstTestCase {
+    public static void main(String[] args) {
+        // Chrome Browser
+        // ChromeDriver driver = new ChromeDriver();
+        // driver.get("https://www.google.com/");
+        // 1. open Chrome Browser
+        WebDriver driver = new ChromeDriver();// upcasting
+        // 2. Go to https://www.google.com/
+        driver.get("https://www.google.com/");
+        // 3. Validate the title is "Google"
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+            System.out.println("Actual Title: " + actualTitle);
+        }
+        // 4. Close the browser
+        driver.quit();// driver.close();
+
+        // Edge Browser
+        WebDriver driverEdge = new EdgeDriver();// upcasting
+        driverEdge.get("https://www.google.com/");
+        String expectedTitleEdge = "Google";
+        String actualTitleEdge = driverEdge.getTitle();
+        if (actualTitleEdge.equals(expectedTitleEdge)) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+            System.out.println("Actual Title: " + actualTitleEdge);
+        }
+        driverEdge.close();
+    }
+}
---
+++ selenium_webdriver/src/test/java/day8_Browser_Navigation_Cmds/ClosingSpecificBrowserWindow.java
@@ -0,0 +1,12 @@
+package day8_Browser_Navigation_Cmds;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClosingSpecificBrowserWindow {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        
+        
+    }
+}
