0f9aa07dd91060e94c0f7f750d18e8c1bea9e32e
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-09-20 15:50:30 +0530

    Waits Done

--- selenium_webdriver/src/test/java/day7/ImplicitWait.java
+++ selenium_webdriver/src/test/java/day7/ImplicitWait.java
@@ -1,5 +1,8 @@
 package day7;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +10,15 @@ public class ImplicitWait {
     public static void main(String[] args) {
         System.out.println("ImplicitWait");
         WebDriver driver = new ChromeDriver();
-        
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("twotabsearchtextbox"))
+                .sendKeys("iphone 16 pro");
+        driver.findElement(By.id("nav-search-submit-button"))
+                .click();
+        driver.quit();
+
     }
 }
