ab56a9cafe7b891454d9a97e4fedf86688770b67
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-09-24 16:48:53 +0530

    day10 first

--- selenium_webdriver/src/test/java/day10_Frames_IFrames_NestedF_iF/HandlingFrames.java
+++ selenium_webdriver/src/test/java/day10_Frames_IFrames_NestedF_iF/HandlingFrames.java
@@ -1,5 +1,16 @@
 package day10_Frames_IFrames_NestedF_iF;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandlingFrames {
-    
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.manage().window().maximize();
+
+        
+
+        driver.quit();
+    }
 }
