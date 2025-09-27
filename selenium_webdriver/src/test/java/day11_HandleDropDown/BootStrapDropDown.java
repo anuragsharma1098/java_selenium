1f282561e4a8809feefdc65b6f88628923c711d9
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-09-27 16:09:14 +0530

    bootstrap dropdown

--- selenium_webdriver/src/test/java/day11_HandleDropDown/BootStrapDropDown.java
+++ selenium_webdriver/src/test/java/day11_HandleDropDown/BootStrapDropDown.java
@@ -1,14 +1,42 @@
 package day11_HandleDropDown;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BootStrapDropDown {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();;
+        driver.manage().window().maximize();
+
+        driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
+
+        driver.findElement(By.xpath("//button[contains(@class, 'multiselect')]")).click();
+
+        // select single option
+        driver.findElement(By.xpath("//input[@value='Java']")).click();
+
+        // capture all the options and find size
+        List<WebElement> options = driver.findElements(By
+                .xpath("//ul[contains(@class, 'multiselect')]//label"));
+        System.out.println("Total no. of options: " + options.size());
+
+        // printing the options from dropdown
+        for (WebElement option : options) {
+            System.out.println(option.getText());
+        }
+
+        // select multiple options
+        for (WebElement option : options) {
+            if (option.getText().equals("Java") || option.getText().equals("Python")) {
+                option.click();
+            }
+        }
+
     }
 }
