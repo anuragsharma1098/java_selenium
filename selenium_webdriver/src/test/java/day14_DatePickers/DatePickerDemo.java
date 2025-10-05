1f1ea6a6f71b202114cf9e54fa47e94ed2b5fce4
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-10-05 21:46:05 +0530

    .

--- selenium_webdriver/src/test/java/day14_DatePickers/DatePickerDemo.java
+++ selenium_webdriver/src/test/java/day14_DatePickers/DatePickerDemo.java
@@ -1,7 +1,48 @@
 package day14_DatePickers;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DatePickerDemo {
     public static void main(String[] args) {
-        
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://jqueryui.com/datepicker/");
+
+        // switch to frame
+        driver.switchTo().frame(0);
+
+        // Method 1: using sendKeys()
+        driver.findElement(By
+                .xpath("//*[@id=\"datepicker\"]"))
+                .sendKeys("10/05/2025"); // MM/DD/YYYY
+
+        // Method 2: using date picker
+        String year = "2025";
+        String month = "December";
+        String day = "05";
+        driver.findElement(By
+                .xpath("//*[@id=\"datepicker\"]")).click();
+        while (true) {
+            String curr_month = driver.findElement(By
+                    .xpath("//span[@class='ui-datepicker-month']"))
+                    .getText();
+            String curr_year = driver.findElement(By
+                    .xpath("//span[@class='ui-datepicker-year']"))
+                    .getText();
+            if (curr_month.equals(month) && curr_year.equals(year)) {
+                break;
+            } else
+            // click on next button
+                driver.findElement(By
+                        .xpath("//*[@id=\"ui-datepicker-div\"]/div/a[2]/span"))
+                        .click();
+
+        }
     }
 }
