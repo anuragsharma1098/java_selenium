bda40f26b82c51f3e518abefa17a8c08c93cd470
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-10-08 22:25:02 +0530

    hands on todo

--- selenium_webdriver/src/test/java/day14_DatePickers/DatePickerHandsOn.java
+++ selenium_webdriver/src/test/java/day14_DatePickers/DatePickerHandsOn.java
@@ -1,7 +1,17 @@
 package day14_DatePickers;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DatePickerHandsOn {
     public static void main(String[] args) {
-        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration
+                .ofSeconds(10));
+        driver.get("https://dummy-tickets.com/buyticket");
+
     }
 }
