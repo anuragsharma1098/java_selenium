81737342e334688edc8447678dee664e452a46ed
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-11-01 15:55:11 +0530

    .

--- selenium_webdriver/src/test/java/day20_DataDrivenTestingWithMSExcel/WritingDataSpecificRowAndCell.java
+++ selenium_webdriver/src/test/java/day20_DataDrivenTestingWithMSExcel/WritingDataSpecificRowAndCell.java
@@ -9,6 +9,15 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WritingDataSpecificRowAndCell {
     public static void main(String[] args) throws Exception {
-
+        FileOutputStream file = new FileOutputStream(
+                "C:\\Users\\anura\\Desktop\\java_selenium\\selenium_webdriver\\testdata\\myfile_specific.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Sheet1");
+        XSSFRow row = sheet.createRow(3);
+        XSSFCell cell = row.createCell(4);
+        cell.setCellValue("Java Selenium Automation");
+        workbook.write(file);
+        workbook.close();
+        file.close();
     }
 }
