386b85bdbe3959511c4c2c88358d758957b5e424
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-11-02 19:20:27 +0530

    testng

--- selenium_webdriver/src/test/java/day22_TestNG/FirstTestCase.java
+++ selenium_webdriver/src/test/java/day22_TestNG/FirstTestCase.java
@@ -9,17 +9,17 @@ import org.testng.annotations.Test;
  */
 
 public class FirstTestCase {
-    @Test
+   @Test(priority=1)
     void openapp() {
         System.out.println("open app...");
     }
 
-    @Test
+    @Test(priority=2)
     void login() {
         System.out.println("login app...");
     }
 
-    @Test
+    @Test(priority=3)
     void logout() {
         System.out.println("logout app...");
     }
